f4a6cc02dd909915393269c7d78dfc0023a2ef81
Kumar Abhimanyu <74586227+universal-innovative@users.noreply.github.com>
2025-12-07 12:58:24 +0530

    table css issue

--- src/components/boxes/BoxTable/BoxesTable.jsx
+++ src/components/boxes/BoxTable/BoxesTable.jsx
@@ -6,32 +6,30 @@ import { formatINR } from "../../../utils/formatting.js";
 
 const BoxesTable = ({ boxes }) => {
   return (
-    <div>
-      <table>
-        <thead>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th className={styles.th}>Receiver</th>
+          <th className={styles.th}>Weight (kg)</th>
+          <th className={styles.th}>Box colour</th>
+          <th className={styles.th}>Destination</th>
+          <th className={styles.th}>Shipping cost (INR)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {boxes.map((b) => (
           <tr>
-            <th className={styles.th}>Receiver</th>
-            <th className={styles.th}>Weight (kg)</th>
-            <th className={styles.th}>Box colour</th>
-            <th className={styles.th}>Destination</th>
-            <th className={styles.th}>Shipping cost (INR)</th>
+            <td className={styles.td}>{b.receiver}</td>
+            <td className={styles.td}>{Number(b.weight).toFixed(2)}</td>
+            <td className={styles.td}>
+              <ColorSwatch rgb={b.colorRgb} code={b.colorRgb} />
+            </td>
+            <td className={styles.td}>{b.country}</td>
+            <td className={styles.td}>{formatINR(b.costInInr)}</td>
           </tr>
-        </thead>
-        <tbody>
-          {boxes.map((b) => (
-            <tr>
-              <td className={styles.td}>{b.receiver}</td>
-              <td className={styles.td}>{Number(b.weight).toFixed(2)}</td>
-              <td className={styles.td}>
-                <ColorSwatch rgb={b.colorRgb} code={b.colorRgb} />
-              </td>
-              <td className={styles.td}>{b.country}</td>
-              <td className={styles.td}>{formatINR(b.costInInr)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
